848cf7f22f3ea7a175d4dd69f6b81d5e37015aef
Tao Zhou <tz7kj@virginia.edu>
2016-02-26 18:58:39 -0500

    use resove to simplify controllers

--- conFusion/www/js/app.js
+++ conFusion/www/js/app.js
@@ -7,89 +7,102 @@
 angular.module('conFusion', ['ionic', 'conFusion.controllers','conFusion.services'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+	$ionicPlatform.ready(function() {
+	// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+	// for form inputs)
+	if (window.cordova && window.cordova.plugins.Keyboard) {
+		cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+	}
+	if (window.StatusBar) {
+	  // org.apache.cordova.statusbar required
+	  StatusBar.styleDefault();
+	}
+});
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+	$stateProvider
 
-  .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/sidebar.html',
-    controller: 'AppCtrl'
-  })
+	.state('app', {
+		url: '/app',
+		abstract: true,
+		templateUrl: 'templates/sidebar.html',
+		controller: 'AppCtrl'
+	})
 
-  .state('app.home', {
-    url: '/home',
-    views: {
-      'mainContent': {
-        templateUrl: 'templates/home.html',
-          controller: 'IndexController'
-      }
-    }
-  })
+	.state('app.home', {
+		url: '/home',
+		views: {
+			'mainContent': {
+				templateUrl: 'templates/home.html',
+				controller: 'IndexController'
+			}
+		}
+	})
 
-  .state('app.aboutus', {
-      url: '/aboutus',
-      views: {
-        'mainContent': {
-          templateUrl: 'templates/aboutus.html',
-          controller:"AboutController"
-        }
-      }
-    })
+	.state('app.aboutus', {
+		url: '/aboutus',
+		views: {
+			'mainContent': {
+				templateUrl: 'templates/aboutus.html',
+				controller:"AboutController"
+			}
+		}
+	})
 
-   .state('app.contactus', {
-      url: '/contactus',
-      views: {
-        'mainContent': {
-          templateUrl: 'templates/contactus.html'
-        }
-      }
-    })
+	.state('app.contactus', {
+		url: '/contactus',
+		views: {
+			'mainContent': {
+				templateUrl: 'templates/contactus.html'
+			}
+		}
+	})
 
-  .state('app.menu', {
-      url: '/menu',
-      views: {
-        'mainContent': {
-          templateUrl: 'templates/menu.html',
-          controller: 'MenuController'
-        }
-      }
-    })
+	.state('app.menu', {
+		url: '/menu',
+		views: {
+			'mainContent': {
+				templateUrl: 'templates/menu.html',
+				controller: 'MenuController'
+			}
+		}
+	})
 
-  .state('app.dishdetails', {
-    url: '/menu/:id',
-    views: {
-      'mainContent': {
-        templateUrl: 'templates/dishdetail.html',
-        controller: 'DishDetailController'
-      }
-    }
-  })
+	.state('app.favorites', {
+		url: '/favorites',
+		views: {
+			'mainContent': {
+				templateUrl: 'templates/favorites.html',
+				controller:'FavoritesController',
+				resolve: {
+					dishes:  ['menuFactory', function(menuFactory){
+						return menuFactory.query();
+					}],
+					favorites: ['favoriteFactory', function(favoriteFactory) {
+						return favoriteFactory.getFavorites();
+					}]
+				}
+			}
+		}
+	})
 
-  .state('app.favorites', {
-      url: '/favorites',
-      views: {
-        'mainContent': {
-          templateUrl: 'templates/favorites.html',
-            controller:'FavoritesController'
-        }
-      }
-    })
+	.state('app.dishdetails', {
+		url: '/menu/:id',
+		views: {
+			'mainContent': {
+				templateUrl: 'templates/dishdetail.html',
+				controller: 'DishDetailController',
+				resolve: {
+					dish: ['$stateParams','menuFactory', function($stateParams, menuFactory){
+						return menuFactory.get({id:parseInt($stateParams.id, 10)});
+					}]
+				}
+			}
+		}
+	});
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/home');
